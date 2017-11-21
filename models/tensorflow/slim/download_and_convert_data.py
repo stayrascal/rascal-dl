cc2bccdf7f1bf1686b057a1cac2b023af9373ef9
Zhiping Wu <zpwu@thoughtworks.com>
2017-11-21 21:50:14 +0800

    format slim mode

--- models/tensorflow/slim/download_and_convert_data.py
+++ models/tensorflow/slim/download_and_convert_data.py
@@ -43,34 +43,30 @@ from datasets import download_and_convert_challenger_ai_scene
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string(
-    'dataset_name',
-    None,
-    'The name of the dataset to convert, one of "cifar10", "flowers", "mnist".')
+tf.app.flags.DEFINE_string('dataset_name', None,
+                           'The name of the dataset to convert, one of "cifar10", "flowers", "mnist".')
 
-tf.app.flags.DEFINE_string(
-    'dataset_dir',
-    None,
-    'The directory where the output TFRecords and temporary files are saved.')
+tf.app.flags.DEFINE_string('dataset_dir', None,
+                           'The directory where the output TFRecords and temporary files are saved.')
 
 
 def main(_):
-  if not FLAGS.dataset_name:
-    raise ValueError('You must supply the dataset name with --dataset_name')
-  if not FLAGS.dataset_dir:
-    raise ValueError('You must supply the dataset directory with --dataset_dir')
+    if not FLAGS.dataset_name:
+        raise ValueError('You must supply the dataset name with --dataset_name')
+    if not FLAGS.dataset_dir:
+        raise ValueError('You must supply the dataset directory with --dataset_dir')
 
-  if FLAGS.dataset_name == 'cifar10':
-    download_and_convert_cifar10.run(FLAGS.dataset_dir)
-  elif FLAGS.dataset_name == 'flowers':
-    download_and_convert_flowers.run(FLAGS.dataset_dir)
-  elif FLAGS.dataset_name == 'challenger_ai_scene':
-    download_and_convert_challenger_ai_scene.run(FLAGS.dataset_dir)
-  elif FLAGS.dataset_name == 'mnist':
-    download_and_convert_mnist.run(FLAGS.dataset_dir)
-  else:
-    raise ValueError(
-        'dataset_name [%s] was not recognized.' % FLAGS.dataset_name)
+    if FLAGS.dataset_name == 'cifar10':
+        download_and_convert_cifar10.run(FLAGS.dataset_dir)
+    elif FLAGS.dataset_name == 'flowers':
+        download_and_convert_flowers.run(FLAGS.dataset_dir)
+    elif FLAGS.dataset_name == 'challenger_ai_scene':
+        download_and_convert_challenger_ai_scene.run(FLAGS.dataset_dir)
+    elif FLAGS.dataset_name == 'mnist':
+        download_and_convert_mnist.run(FLAGS.dataset_dir)
+    else:
+        raise ValueError('dataset_name [%s] was not recognized.' % FLAGS.dataset_name)
+
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
