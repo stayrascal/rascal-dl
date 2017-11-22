80f434a742d5968f94e0b7370b21d1a491764c22
Zhiping Wu <zpwu@thoughtworks.com>
2017-11-22 21:46:40 +0800

    format slim mode

--- models/tensorflow/slim/datasets/flowers.py
+++ models/tensorflow/slim/datasets/flowers.py
@@ -41,7 +41,7 @@ _ITEMS_TO_DESCRIPTIONS = {
 }
 
 
-def get_split(split_name, dataset_dir, file_pattern=None, reader=None):
+def get_split(split_name, dataset_dir, file_pattern=_FILE_PATTERN, reader=tf.TFRecordReader):
     """Gets a dataset tuple with instructions for reading flowers.
 
     Args:
@@ -61,19 +61,12 @@ def get_split(split_name, dataset_dir, file_pattern=None, reader=None):
     if split_name not in SPLITS_TO_SIZES:
         raise ValueError('split name %s was not recognized.' % split_name)
 
-    if not file_pattern:
-        file_pattern = _FILE_PATTERN
     file_pattern = os.path.join(dataset_dir, file_pattern % split_name)
-
-    # Allowing None in the signature so that dataset_factory can use the default.
-    if reader is None:
-        reader = tf.TFRecordReader
 
     keys_to_features = {
         'image/encoded': tf.FixedLenFeature((), tf.string, default_value=''),
         'image/format': tf.FixedLenFeature((), tf.string, default_value='png'),
-        'image/class/label': tf.FixedLenFeature(
-            [], tf.int64, default_value=tf.zeros([], dtype=tf.int64)),
+        'image/class/label': tf.FixedLenFeature([], tf.int64, default_value=tf.zeros([], dtype=tf.int64)),
     }
 
     items_to_handlers = {
@@ -87,11 +80,10 @@ def get_split(split_name, dataset_dir, file_pattern=None, reader=None):
     if dataset_utils.has_labels(dataset_dir):
         labels_to_names = dataset_utils.read_label_file(dataset_dir)
 
-    return slim.dataset.Dataset(
-        data_sources=file_pattern,
-        reader=reader,
-        decoder=decoder,
-        num_samples=SPLITS_TO_SIZES[split_name],
-        items_to_descriptions=_ITEMS_TO_DESCRIPTIONS,
-        num_classes=_NUM_CLASSES,
-        labels_to_names=labels_to_names)
+    return slim.dataset.Dataset(data_sources=file_pattern,
+                                reader=reader,
+                                decoder=decoder,
+                                num_samples=SPLITS_TO_SIZES[split_name],
+                                items_to_descriptions=_ITEMS_TO_DESCRIPTIONS,
+                                num_classes=_NUM_CLASSES,
+                                labels_to_names=labels_to_names)
